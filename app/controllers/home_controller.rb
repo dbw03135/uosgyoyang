f629a1de12f139d2853c3978222dba74fa2485ef
Tickle <paletta1014@gmail.com>
2019-08-09 00:07:26 +0900

    수강평가 기초 개발

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -20,4 +20,35 @@ class HomeController < ApplicationController
     @subj=doc.xpath('//list')
     @subj2=doc2.xpath('//list')
   end
+  def estimate
+    @year = 2018
+    @term = 2
+    results = []
+    
+    estimates = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcsLecturerEstimateResultInq.oapi?apiKey=201605329QCU92763&year=#{@year}&term=A#{@term}0&subjectNm=asdfasdfasdf&subjectNo=#{params[:id]}"),nil,'EUC-KR')
+    estimates = estimates.xpath('//list')
+    
+    estimates.each do |node|
+      puts node.xpath('prof_nm').text.strip
+
+      if node.xpath('prof_nm').text.strip == params[:prof_nm]
+        results << node.xpath('raw_score').text.strip.to_f
+      end
+    end
+
+    if results.length != 0
+      @average = results.inject{ |sum, el| sum + el }.to_f / results.size
+      @average = (@average*100).round / 100.0
+    else
+      @average = -1
+    end
+
+    @json = {'average'=> @average}
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @json }
+    end
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get '/all' => 'home#all'
   get '/mungwa' => 'home#mungwa'
   get '/major' => 'home#major'
+  get '/estimate/:id' => 'home#estimate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
